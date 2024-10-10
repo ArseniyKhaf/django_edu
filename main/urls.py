a8937074810ce6a959f7a558004a4910e9188e85
Iskan09 <152530058+Iskan09@users.noreply.github.com>
2024-10-10 17:48:11 +0300

    initial

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
+from django.urls import path
 from main import views
 
 urlpatterns = [
     path('', views.index),
     path("prof/", views.profil)
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
